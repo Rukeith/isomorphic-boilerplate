23d038ef1bea4cc36845e2d06ab38adab74032b1
alichen <ali322@gmail.com>
2015-08-19 22:17:39 +0800

    move component to shared

--- server/ctrl/main.js
+++ server/ctrl/main.js
@@ -1,7 +1,7 @@
 'use strict'
 require('node-jsx').install();
 var React = require("react");
-var User = React.createFactory(require("../../client/index/index.jsx"));
+var IndexComponent = React.createFactory(require("../../shared/index/component.jsx"));
 
 
 let index = function(req, res) {
@@ -23,7 +23,7 @@ let index = function(req, res) {
             count: 6
         }
     };
-    var markup = React.renderToString(User({
+    var markup = React.renderToString(IndexComponent({
         initialData
     }));
     res.render("index", {
